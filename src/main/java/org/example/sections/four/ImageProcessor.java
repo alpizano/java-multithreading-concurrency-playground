818c59e06d0e70151f1699edda8490c61df52b73
alpizano <apizano@purdue.edu>
2021-01-01 19:19:00 -0600

    add creatergbfromcolors method

--- src/main/java/org/example/sections/four/ImageProcessor.java
+++ src/main/java/org/example/sections/four/ImageProcessor.java
@@ -1,13 +1,31 @@
 package org.example.sections.four;
 
 public class ImageProcessor {
+
+    /**
+     *
+     * @param red value from 0 to 255
+     * @param green value from 0 to 255
+     * @param blue value from 0 to 255
+     * @return build compound pixel rgb value
+     */
+    public static int createRGBFromColors(int red, int green, int blue) {
+        int rgb = 0;
+
+        rgb |= blue;
+        rgb |= green << 8; // shift green left 8 bits, then add
+        rgb |= red << 16; // shift red left 16 bits, then add
+
+        // set alpha value to 255 (opaque)
+        return (rgb |= 0xFF000000);
+    }
     /**
      *
      * @param rgb the four byte rgb pixel value from image
      * @return the bitwise AND of rgb pixel and bitmask
      */
     public static int getRed(int rgb) {
-        return (rgb & 0x00FF0000);
+        return (rgb & 0x00FF0000) >> 16;
     }
 
     /**
@@ -16,7 +34,8 @@ public class ImageProcessor {
      * @return the bitwise AND of rgb pixel and bitmask
      */
     public static int getGreen(int rgb) {
-        return (rgb & 0x0000FF00);
+        // shift bits one byte to the right
+        return (rgb & 0x0000FF00) >> 8;
     }
 
     /**
@@ -29,6 +48,12 @@ public class ImageProcessor {
 
     }
     public static void main(String[] args) {
+        int seventeen = 0x11; // 0001 0001
+        int bitmask = 0xFF; // 1111 1111
+        int result = seventeen & bitmask;
+
+        System.out.println(result); // 17
+        System.out.println(result >> 4); // 0001 0001 (17) => (shift right 4) 0000 0001 (1)
 
     }
 }
