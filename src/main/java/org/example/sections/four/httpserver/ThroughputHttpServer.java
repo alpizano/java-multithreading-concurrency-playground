5bb6e740a48da0b1908c53dedfa0aadb7f238a38
kbrv37 <apizano@purdue.edu>
2021-01-09 11:42:54 -0600

    implement http handler and word count

--- src/main/java/org/example/sections/four/httpserver/ThroughputHttpServer.java
+++ src/main/java/org/example/sections/four/httpserver/ThroughputHttpServer.java
@@ -5,9 +5,12 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class ThroughputHttpServer {
     private static final String INPUT_FILE = "resources/war_and_peace.txt";
@@ -15,6 +18,7 @@ public class ThroughputHttpServer {
 
     public static void main(String[] args) throws IOException {
         String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
+        System.out.println(text);
         startServer(text);
     }
 
@@ -22,6 +26,7 @@ public class ThroughputHttpServer {
         // create http server
         HttpServer server = HttpServer.create(new InetSocketAddress(8000),0);
         server.createContext("/search", new WordCountHandler(text));
+        Executor executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
         server.setExecutor(executor);
         server.start();
     }
@@ -36,7 +41,39 @@ public class ThroughputHttpServer {
 
         @Override
         public void handle(HttpExchange httpExchange) throws IOException {
+            String query = httpExchange.getRequestURI().getQuery();
+            String[] keyValue = query.split("=");
+            String action = keyValue[0];
+            String word = keyValue[1];
+            if(!action.equals("word")) {
+                httpExchange.sendResponseHeaders(400,0);
+                return;
+            }
 
+            long count = countWord(word);
+            // convert to byte array that can be serialized
+            byte[] response = Long.toString(count).getBytes();
+            httpExchange.sendResponseHeaders(200,response.length);
+            OutputStream outputStream = httpExchange.getResponseBody();
+            outputStream.write(response);
+            outputStream.close();
         }
+
+        private long countWord(String word) {
+            long count = 0;
+            int index = 0;
+
+            while(index >= 0) {
+                index = text.indexOf(word, index);
+
+                if(index >=0) {
+                    count++;
+                    index++;
+                }
+            }
+            return count;
+        }
+
+
     }
 }
