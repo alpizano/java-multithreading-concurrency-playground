a73590f36fbf2f049d1e1cd7550f164c099721e4
kbrv37 <apizano@purdue.edu>
2021-01-18 20:28:53 -0600

    adding inventory database read write lock ex

---
+++ src/main/java/org/example/sections/seven/readwritelock/Main.java
@@ -0,0 +1,24 @@
+package org.example.sections.seven.readwritelock;
+
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static class InventoryDatabase {
+        private TreeMap<Integer, Integer> priceToCountMap = new TreeMap<>();
+
+        public int getNumberOfItemsInPriceRange(int lowerBound, int upperBound) {
+            return 0;
+        }
+
+        public void addItem(int price) {
+
+        }
+
+        public void removeItem(int price) {
+
+        }
+    }
+}
